6b1e7156ff8b76c63d9a0f46f589b65895f54a2b
Adam Newgas <boris@boristhebrave.com>
2018-07-16 21:48:53 +0100

    Add BorderConstraintTest

---
+++ DeBroglie.Test/BorderConstraintTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace DeBroglie.Test
+{
+    [TestFixture]
+    public class BorderConstraintTest
+    {
+        [Test]
+        public void TestBorderConstraint()
+        {
+            var a = new int[,]{
+                 {1, 0, 0},
+                 {0, 1, 1},
+                 {0, 1, 1},
+            };
+
+            var model = AdjacentModel.Create(a, true);
+            var propagator = new TilePropagator(model, new Topology(10, 10, false), true, constraints: new[] {
+                new BorderConstraint{
+                    Tile = new Tile(0),
+                }
+            });
+            var status = propagator.Run();
+            Assert.AreEqual(CellStatus.Decided, status);
+            var result = propagator.ToValueArray<int>().ToArray2d();
+            Assert.AreEqual(0, result[0, 0]);
+            Assert.AreEqual(0, result[9, 0]);
+            Assert.AreEqual(0, result[0, 9]);
+            Assert.AreEqual(0, result[9, 9]);
+
+        }
+    }
+}
