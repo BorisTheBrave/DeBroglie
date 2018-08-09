b12ea014061422b04208b27cf93b2cff28fe41e8
Adam Newgas <boris@boristhebrave.com>
2018-08-09 21:52:57 +0100

    Add missing file

---
+++ DeBroglie/Topo/RaggedTopoArray2D.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace DeBroglie.Topo
+{
+    internal class RaggedTopoArray2D<T> : ITopoArray<T>
+    {
+        private readonly T[][] values;
+
+        public RaggedTopoArray2D(T[][] values, bool periodic)
+        {
+            var height = values.Length;
+            var width = values.Max(a => a.Length);
+            Topology = new Topology(
+                width,
+                height,
+                periodic);
+            this.values = values;
+        }
+
+        public RaggedTopoArray2D(T[][] values, Topology topology)
+        {
+            Topology = topology;
+            this.values = values;
+        }
+
+        public Topology Topology { get; private set; }
+
+        public T Get(int x, int y, int z)
+        {
+            return values[y][x];
+        }
+
+        public T Get(int index)
+        {
+            int x, y, z;
+            Topology.GetCoord(index, out x, out y, out z);
+            return Get(x, y, z);
+        }
+    }
+}
