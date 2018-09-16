dd6da81194b4705bb8bad9d5c687d6009451fbe3
Adam Newgas <boris@boristhebrave.com>
2018-09-16 17:22:17 +0100

    Start adding samples.

---
+++ samples/grass/README.md
@@ -0,0 +1,3 @@
+Simple path and grass tileset, with several DeBroglie features enabled at once.
+
+Graphics from https://opengameart.org/content/overworld-grass-biome (CC0)
---
+++ samples/grass/grass_biome.tsx
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass_biome" tilewidth="16" tileheight="16" tilecount="252" columns="12">
+ <image source="overworld_tileset_grass.png" width="192" height="336"/>
+ <terraintypes>
+  <terrain name="Grass" tile="13"/>
+  <terrain name="Water" tile="0"/>
+  <terrain name="Forest" tile="0"/>
+  <terrain name="Swampgrass" tile="0"/>
+  <terrain name="Swamp" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0" probability="20"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,0"/>
+ <tile id="3" terrain="0,0,0,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="25" terrain="0,0,0,0"/>
+ <tile id="26" terrain="0,0,0,0"/>
+ <tile id="37" terrain="0,0,0,0"/>
+ <tile id="38" terrain="0,0,0,0"/>
+ <tile id="48" terrain="0,0,0,1"/>
+ <tile id="49" terrain="0,0,1,1"/>
+ <tile id="50" terrain="0,0,1,0"/>
+ <tile id="51" terrain="3,3,3,4"/>
+ <tile id="52" terrain="3,3,4,4"/>
+ <tile id="53" terrain="3,3,4,3"/>
+ <tile id="57" terrain="0,0,0,2"/>
+ <tile id="58" terrain="0,0,2,2"/>
+ <tile id="59" terrain="0,0,2,0"/>
+ <tile id="60" terrain="0,1,0,1"/>
+ <tile id="61" terrain="1,1,1,1"/>
+ <tile id="62" terrain="1,0,1,0"/>
+ <tile id="63" terrain="3,4,3,4"/>
+ <tile id="64" terrain="4,4,4,4"/>
+ <tile id="65" terrain="4,3,4,3"/>
+ <tile id="69" terrain="0,2,0,2"/>
+ <tile id="70" terrain="2,2,2,2"/>
+ <tile id="71" terrain="2,0,2,0"/>
+ <tile id="72" terrain="0,1,0,0"/>
+ <tile id="73" terrain="1,1,0,0"/>
+ <tile id="74" terrain="1,0,0,0"/>
+ <tile id="75" terrain="3,4,3,3"/>
+ <tile id="76" terrain="4,4,3,3"/>
+ <tile id="77" terrain="4,3,3,3"/>
+ <tile id="81" terrain="0,2,0,0"/>
+ <tile id="82" terrain="2,2,0,0"/>
+ <tile id="83" terrain="2,0,0,0"/>
+ <tile id="84" terrain="1,1,1,0"/>
+ <tile id="85" terrain="1,1,0,1"/>
+ <tile id="87" terrain="4,4,4,3"/>
+ <tile id="88" terrain="4,4,3,4"/>
+ <tile id="89" terrain="3,3,3,3"/>
+ <tile id="93" terrain="2,2,2,0"/>
+ <tile id="94" terrain="2,2,0,2"/>
+ <tile id="96" terrain="1,0,1,1"/>
+ <tile id="97" terrain="0,1,1,1"/>
+ <tile id="99" terrain="4,3,4,4"/>
+ <tile id="100" terrain="3,4,4,4"/>
+ <tile id="105" terrain="2,0,2,2"/>
+ <tile id="106" terrain="0,2,2,2"/>
+ <tile id="123" terrain="3,3,3,3"/>
+ <tile id="124" terrain="3,3,3,3"/>
+ <tile id="125" terrain="3,3,3,3"/>
+ <tile id="135" terrain="3,3,3,3"/>
+ <tile id="136" terrain="3,3,3,3"/>
+ <tile id="137" terrain="3,3,3,3"/>
+</tileset>
