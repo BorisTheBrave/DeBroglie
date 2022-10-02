54133fc2697deac382fe1cb919facb5ed096bb34
Boris The Brave <boris@boristhebrave.com>
2022-10-02 20:46:11 +0100

    Removed broken script.

--- samples/mxgmn/run.ps1
+++
@@ -1,7 +0,0 @@
-$DeBroglie="..\..\DeBroglie.Console\bin\Debug\DeBroglie.Console.exe"
-Get-ChildItem "." -Filter *.json |
-Foreach-Object {
-    if ($_.Name -ne "schema.json") {
-        & $DeBroglie $_
-    }
-}
